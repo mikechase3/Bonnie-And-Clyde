f7cff5c6ab93cb7593123ee01fec8d7f690a6638
Michael Chase <me@mchase.me>
2019-07-28 22:41:52 -0400

    Update Arduino_5V_Test.ino

--- Arduino_5V_Test/Arduino_5V_Test.ino
+++ Arduino_5V_Test/Arduino_5V_Test.ino
@@ -8,7 +8,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(3,HIGH);
-  delay(500);
+  delay(1500);
   digitalWrite(3,LOW);
-  delay(500);
+  delay(750);
 }
