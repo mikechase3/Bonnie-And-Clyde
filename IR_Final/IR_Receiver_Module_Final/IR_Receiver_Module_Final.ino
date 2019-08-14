7d46a8b21c3bfbfe034a966160a207def8b7abe7
Krimpenfort23 <evankrimp@gmail.com>
2019-08-14 00:53:23 -0400

    After Merge

--- IR_Final/IR_Receiver_Module_Final/IR_Receiver_Module_Final.ino
+++ IR_Final/IR_Receiver_Module_Final/IR_Receiver_Module_Final.ino
@@ -2,13 +2,16 @@
 #include <IRremote.h>
 #include <IRremoteInt.h>
 #include <ir_Lego_PF_BitStreamEncoder.h>
+#include <LiquidCrystal.h>
 
+// global variables
 const int receiver = 7;
 int play_pause = 0;
 double time_left = 0.0;
 bool isPreset = false;
 
 // declare objects
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 IRrecv irrecv(receiver); //Create an instance of 'irrecv'
 decode_results results; //Create instance of 'decode_results'
 
@@ -18,17 +21,21 @@ void translateIR() {
   switch(results.value) {
     case 0xFFA25D:
       Serial.println("POWER");
+      lcd.clear();
+      lcd.print("POWER");
       digitalWrite(8,LOW);
       isPreset = false;
       time_left = 0;
       play_pause = 0;
       break;
-    case 0xFFE21D: Serial.println("FUNC/STOP"); break;
-    case 0xFF629D: Serial.println("VOL+"); break;
-    case 0xFF22DD: Serial.println("Fast Back"); break;
+    case 0xFFE21D: Serial.println("FUNC/STOP"); lcd.clear(); lcd.print("FUNC/STOP"); break;
+    case 0xFF629D: Serial.println("VOL+"); lcd.clear(); lcd.print("VOL+"); break;
+    case 0xFF22DD: Serial.println("FAST BACK"); lcd.clear(); lcd.print("FAST BACK"); break;
     case 0xFF02FD:
       if (!isPreset) {
         Serial.println("PAUSE/PLAY");
+        lcd.clear();
+        lcd.print("PAUSE/PLAY");
         if (play_pause == 0) {
           play_pause = 1;
           digitalWrite(8,HIGH);
@@ -42,16 +49,18 @@ void translateIR() {
         Serial.println("PRESET ON - PRESS POWER");
       }
       break;
-    case 0xFFC23D: Serial.println("FAST FORWARD"); break;
-    case 0xFFE01F: Serial.println("DOWN"); break;
-    case 0xFFA857: Serial.println("VOL -"); break;
-    case 0xFF906F: Serial.println("UP"); break;
-    case 0xFF9867: Serial.println("EQ"); break;
-    case 0xFFB04F: Serial.println("ST/REPT"); break;
+    case 0xFFC23D: Serial.println("FAST FORWARD"); lcd.clear(); lcd.print("FAST FORWARD"); break;
+    case 0xFFE01F: Serial.println("DOWN"); lcd.clear(); lcd.print("DOWN"); break;
+    case 0xFFA857: Serial.println("VOL -"); lcd.clear(); lcd.print("VOL-"); break;
+    case 0xFF906F: Serial.println("UP"); lcd.clear(); lcd.print("UP"); break;
+    case 0xFF9867: Serial.println("EQ"); lcd.clear(); lcd.print("EQ"); break;
+    case 0xFFB04F: Serial.println("ST/REPT"); lcd.clear(); lcd.print("ST/REPT"); break;
     // Start Numbers and Digits
-    case 0xFF6897: Serial.println("0"); break;
+    case 0xFF6897: Serial.println("0"); lcd.clear(); lcd.print("0"); break;
     case 0xFF30CF:
       Serial.println("1 - 10 seconds");
+      lcd.clear();
+      lcd.print("10 SECONDS");
       digitalWrite(8,HIGH);
       isPreset = true;
       play_pause = 0;
@@ -59,6 +68,8 @@ void translateIR() {
       break;
     case 0xFF18E7:
       Serial.println("2 - 60 seconds - 1 minute");
+      lcd.clear();
+      lcd.print("60 SECONDS");
       digitalWrite(8,HIGH);
       isPreset = true;
       play_pause = 0;
@@ -66,6 +77,8 @@ void translateIR() {
       break;
     case 0xFF7A85:
       Serial.println("3 - 120 seconds - 2 minutes");
+      lcd.clear();
+      lcd.print("120 SECONDS");
       digitalWrite(8,HIGH);
       isPreset = true;
       play_pause = 0;
@@ -73,6 +86,8 @@ void translateIR() {
       break;
     case 0xFF10EF:
       Serial.println("4 - 300 seconds - 5 minutes");
+      lcd.clear();
+      lcd.print("300 SECONDS");
       digitalWrite(8,HIGH);
       isPreset = true;
       play_pause = 0;
@@ -80,6 +95,8 @@ void translateIR() {
       break;
     case 0xFF38C7:
       Serial.println("5 - 900 seconds - 15 minutes");
+      lcd.clear();
+      lcd.print("900 SECONDS");
       digitalWrite(8,HIGH);
       isPreset = true;
       play_pause = 0;
@@ -87,6 +104,8 @@ void translateIR() {
       break;
     case 0xFF5AA5:
       Serial.println("6 - 1800 seconds - 30 minutes");
+      lcd.clear();
+      lcd.print("1800 SECONDS");
       digitalWrite(8,HIGH);
       isPreset = true;
       play_pause = 0;
@@ -94,6 +113,8 @@ void translateIR() {
       break;
     case 0xFF42BD:
       Serial.println("7 - 3600 seconds - 60 minutes - 1 hour");
+      lcd.clear();
+      lcd.print("3600 SECONDS");
       digitalWrite(8,HIGH);
       isPreset = true;
       play_pause = 0;
@@ -101,6 +122,8 @@ void translateIR() {
       break;
     case 0xFF4AB5:
       Serial.println("8 - 7200 seconds - 120 minutes - 2 hours");
+      lcd.clear();
+      lcd.print("7200 SECONDS");
       digitalWrite(8,HIGH);
       isPreset = true;
       play_pause = 0;
@@ -108,6 +131,8 @@ void translateIR() {
       break;
     case 0xFF52AD:
       Serial.println("9 - 86400 seconds - 720 minutes - 12 hours");
+      lcd.clear();
+      lcd.print("86400 SECONDS");
       digitalWrite(8,HIGH);
       isPreset = true;
       play_pause = 0;
@@ -122,6 +147,29 @@ void translateIR() {
   delay(500);
 }
 
+void refresh() {
+  lcd.clear();
+
+  // first line
+  lcd.setCursor(0,0);
+  lcd.print("Door is ");
+  lcd.setCursor(8,0);
+  if (!isPreset || play_pause == 1) {
+    lcd.print("LOCKED");
+  }
+  else {
+    lcd.print("UNLOCKED");
+  }
+
+  // second line
+  lcd.setCursor(0,1);
+  if (isPreset) {
+    lcd.print("Lock in ");
+    lcd.setCursor(8,1);
+    lcd.print(String(time_left)); // TODO Check this
+  }
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -130,6 +178,15 @@ void setup() {
   pinMode(8,OUTPUT);
   pinMode(10,OUTPUT);
   digitalWrite(10,HIGH);
+
+  // lcd setup
+  lcd.begin(16,2); //Initializes the baord as 2 rows 16 across.
+  lcd.setCursor(0,0);
+  lcd.print("Welcome to")
+  lcd.setCursor(0,1);
+  lcd.print("Bonnie & Clyde");
+  delay(3000);
+  refresh();
 }
 
 void loop() {
@@ -150,4 +207,5 @@ void loop() {
       delay(500);
     }
   }
+  refresh();
 }
